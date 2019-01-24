96d20f55762cdcd001d251ea17948ed27ecd50d0
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-01-24 16:50:46 -0500

    Working code.

--- src/com/company/BookDemo.java
+++ src/com/company/BookDemo.java
@@ -11,42 +11,58 @@ public class BookDemo {
         boolean anotherBook = true;
         int answer = 0;
         int index = 0;
+        int userInput = 0;
 
 
-        
         do {
+            System.out.print("What do you want to do? 1) See how many books. 2) Add a book.  3) Exit");
+            userInput = myScanner.nextInt();
 
-            Book myBook = new Book();
+            switch (userInput) {
+                case 1:
+                    System.out.println(myLibrary.numberOfBooksInLibrary());
+                    anotherBook = true;
+                    break;
+                case 2:
+                    boolean addBook = true;
+                    do {
 
-            System.out.println("\nWhat is the book title?");
-            myBook.setBookTitle(myScanner.next());
+                        Book myBook = new Book();
 
-            System.out.println("\nWhat is the book author?");
-            myBook.setBookAuthor(myScanner.next());
+                        System.out.println("\nWhat is the book title?");
+                        myBook.setBookTitle(myScanner.next());
 
-            System.out.println("\nWhat is the book genre?");
-            myBook.setGenre(myScanner.next());
+                        System.out.println("\nWhat is the book author?");
+                        myBook.setBookAuthor(myScanner.next());
 
-            System.out.println("\nWhat is the year of publication?");
-            myBook.setYearOfPublication(myScanner.nextInt());
+                        System.out.println("\nWhat is the book genre?");
+                        myBook.setGenre(myScanner.next());
 
-            System.out.println("\nWould you like to add another book? 1 for yes.  2 for no.");
-            answer = myScanner.nextInt();
+                        System.out.println("\nWhat is the year of publication?");
+                        myBook.setYearOfPublication(myScanner.nextInt());
 
-            myLibrary.allBooks[index] = myBook;
+                        System.out.println("\nWould you like to add another book? 1 for yes.  2 for no.");
+                        answer = myScanner.nextInt();
 
-            if (answer == 1) {
-                index++;
-            } else if (answer == 2) {
-                anotherBook = false;
-            } else {
-                System.out.println("Try again.");
+                        myLibrary.allBooks[index] = myBook;
+
+                        if (answer == 1) {
+                            index++;
+                        } else if (answer == 2) {
+                            addBook = false;
+                        } else {
+                            System.out.println("Try again.");
+                        }
+
+                    } while (addBook);
+                    break;
+
+                case 3:
+                    System.out.println("You've decided to exit.");
+                    anotherBook = false;
+                    break;
+
             }
-
         } while (anotherBook);
-
-        //System.out.println(myLibrary.numberOfBooksInLibrary());
-
-
     }
 }
